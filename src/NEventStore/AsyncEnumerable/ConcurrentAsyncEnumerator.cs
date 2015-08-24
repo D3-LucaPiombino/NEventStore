e2c5634eb8fcce28e4056d9e1e62b574f43734fb
D3-LucaPiombino <luca.piombino@deltatre.com>
2015-08-24 16:31:31 +0200

    Implemented Dispose to handle interrupted enumerations (Single, FirstOrDefault, Filtered, etc...)

--- src/NEventStore/AsyncEnumerable/ConcurrentAsyncEnumerator.cs
+++ src/NEventStore/AsyncEnumerable/ConcurrentAsyncEnumerator.cs
@@ -19,15 +19,19 @@ namespace ALinq
             Interlocked.Exchange(ref _tcs, new TaskCompletionSource<T>());
             return result;
         }
+
+
     }
 
-    internal sealed class ConcurrentAsyncEnumerator<T> : IAsyncEnumerator<T>
+    internal sealed class ConcurrentAsyncEnumerator<T> : IAsyncEnumerator<T>, IDisposable
     {
+
         private readonly Func<ConcurrentAsyncEnumerator<T>, Task> _producer;
         private readonly Signal<bool> _moveNextSignal = new Signal<bool>();
         private readonly Signal<bool> _producerResult = new Signal<bool>();
         private Task _producerTask;
         private T _current;
+        private bool _disposed;
 
         T IAsyncEnumerator<T>.Current
         {
@@ -43,13 +47,13 @@ namespace ALinq
         {
             try
             {
-                if(_producerTask == null)
+                if (_producerTask == null)
                 {
                     _producerTask = _producer(this);
                 }
 
                 var hasNext = await SignalProducer().ConfigureAwait(false);
-                if(!hasNext)
+                if (!hasNext)
                 {
                     // Marshal exceptions and cancellation back to the consumer
                     await _producerTask.ConfigureAwait(false);
@@ -75,23 +79,36 @@ namespace ALinq
             // Not that after the first time, this will be always signalled.
             // This is only necessary to synchronize the enumerator when the 
             // producer is started.
-            await _moveNextSignal.Wait().ConfigureAwait(false);
-            _current = value;
-            _producerResult.Set(true);
+            if (!_disposed)
+            {
+                await _moveNextSignal.Wait().ConfigureAwait(false);
+                _current = value;
+                _producerResult.Set(true);
+                // Now, wait for the consumer to call the next MoveNext().
+                // This will guarantee that _current instance is valid until
+                // the consumer(s) are done.
+                await _moveNextSignal.Wait().ConfigureAwait(false);
+                _moveNextSignal.Set();
+            }
 
-            // Now, wait for the consumer to call the next MoveNext().
-            // This will guarantee that _current instance is valid until
-            // the consumer(s) are done.
-            await _moveNextSignal.Wait().ConfigureAwait(false);
-            _moveNextSignal.Set();
         }
 
         private async Task EndOfStream()
         {
-            // Wait for the consumer to pull (call MoveNext()).
-            await _moveNextSignal.Wait().ConfigureAwait(false);
+            if (!_disposed)
+            {
+                // Wait for the consumer to pull (call MoveNext()).
+                await _moveNextSignal.Wait().ConfigureAwait(false);
+                _producerResult.Set();
+                // There no need here to weird stuff with _moveNextSignal :)
+            }
+        }
+
+        public void Dispose()
+        {
+            _disposed = true;
+            _moveNextSignal.Set();
             _producerResult.Set();
-            // There no need here to weird stuff with _moveNextSignal :)
         }
 
         internal ConcurrentAsyncEnumerator(Func<ConcurrentAsyncProducer<T>, Task> producerFunc)
