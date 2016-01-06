da3e3e26ba5e4281bfdb2768aeeb10001689fce3
Luca Piombino <luca.piombino@hotmail.com>
2016-01-06 03:10:41 +0100

    more troubleshooting

--- build.cmd
+++ build.cmd
@@ -1,9 +1,14 @@
 
 cls
 
+echo %CD%
+
 pushd src
+echo %CD%
 ..\build\tools\nuget\nuget.exe restore
+echo %CD%
 popd
 
-build\tools\nuget\nuget.exe restore build\tools\build.project.json -OutputDirectory "build\tools"
-build\tools\FAKE\4.12.0\tools\fake.exe build.fsx %*
+
+rem build\tools\nuget\nuget.exe restore build\tools\build.project.json -OutputDirectory "build\tools"
+rem build\tools\FAKE\4.12.0\tools\fake.exe build.fsx %*
