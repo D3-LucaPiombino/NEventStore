9ed6f650dfb942c48def77187eebdc2134d1d133
Luca Piombino <luca.piombino@hotmail.com>
2016-01-10 04:56:57 +0100

    Cleaned a bit the code in common.fsx

--- build/common.fsx
+++ build/common.fsx
@@ -11,45 +11,44 @@ open System.Xml.Linq
 
 module Nuget3 =
 
+    type private DependencyType =
+    | Default
+    | Build
+    | Unknown
+
     let getProjectDependencies (projectJsonFile:string) = 
+
         let json = JsonValue.Load( new StreamReader(projectJsonFile))
         let deps = json.GetProperty("dependencies")
     
-
-        let(|PkgVersion|_|) x =
+        let(|VersionProperty|_|) x =
             match x with
             | ("version", JsonValue.String version ) -> Some(version)
             | _ -> None
-
-    
-        let(|RuntimeDep|_|) x =
+        
+        let(|TypeProperty|_|) x =
             match x with
-            | ("type", JsonValue.String "default" ) -> Some(RuntimeDep)
+            | ("type", JsonValue.String "build"   ) -> Some(Build)
+            | ("type", JsonValue.String "default" ) -> Some(Default)
             | _                                     -> None
 
-        let(|BuildDep|_|) x =
-            match x with
-            | ("type", JsonValue.String "build"   ) -> Some(BuildDep)
-            | _                                     -> None
-
-    
-
+        
         let rec computeRuntimeDependencies packageId jsonValue =
             match jsonValue with
             | JsonValue.String version  -> Some (packageId , version)
             | JsonValue.Record properties    -> 
-                let rec scanProperties propertylist packageType packageVersion =
-                    match propertylist with
-                    | jsonProperty::remainingProperties -> 
+                let rec scanProperties properties packageType packageVersion =
+                    match properties with
+                    | jsonProperty::tail -> 
                         match (jsonProperty, packageType, packageVersion) with
-                        | ( RuntimeDep         , _          , Some(version) ) -> Some (packageId , version)
-                        | ( PkgVersion(version), Some(true) , _             ) -> Some (packageId , version)
-                        | ( PkgVersion(version), _          , _             ) -> scanProperties remainingProperties packageType (Some version) // look for "type"
-                        | ( RuntimeDep         , _          , None          ) -> scanProperties remainingProperties (Some true) packageVersion // look for "version"
-                        | ( BuildDep           , _          , _             ) -> None                                                          // discard this
-                        | _                                                   -> scanProperties remainingProperties packageType packageVersion
+                        | ( TypeProperty(Default)   , Unknown    , Some(version) ) -> Some (packageId , version)
+                        | ( VersionProperty(version), Default    , _             ) -> Some (packageId , version)
+                        | ( VersionProperty(version), Unknown    , _             ) -> scanProperties tail packageType (Some version) // look for "type"
+                        | ( TypeProperty(Default)   , Unknown    , None          ) -> scanProperties tail Default packageVersion     // look for "version"
+                        | ( TypeProperty(Build)     , Unknown    , _             ) -> None                                           // discard this
+                        | _                                                        -> scanProperties tail packageType packageVersion
                     | [] -> None
-                scanProperties (Array.toList properties) None None
+                scanProperties (Array.toList properties) Unknown None
             | _ -> None
 
         deps
